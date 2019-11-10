1d77ed5976c92350419ec55537bbb32fcae10ddc
Iuliia Shevchenko <ishevchenko@admins-MacBook-Pro-3.local>
2019-11-10 17:28:23 +0100

    top secret

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ app = flask.Flask(__name__)
 def main():
 	return flask.render_template('main.html', title="Yulia's Portfolio")
 
+@app.route('/project')
+def project():
+	return flask.render_template('project.html', title="Yulia's Portfolio")
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
     #app.run()	
