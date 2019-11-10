ab4c3a0b78fb98d386f5e988d00a4c856fccbd94
Sathyan Murugan <sathyan@uber.com>
2019-11-10 17:03:28 +0100

    adding jinja templating

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = flask.Flask(__name__)
 
 @app.route('/')
 def main():
-	return "Hello"
+	return flask.render_template('main.html', title="Yulia's Portfolio")
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0', port=8080)
-    app.run()
+    app.run(debug=True, host='0.0.0.0', port=8080)
+    #app.run()	
