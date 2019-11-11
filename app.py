a1034062f072d5e3323283d4724745ed3f941a31
Sathyan Murugan <sathyan@uber.com>
2019-11-11 19:38:11 +0100

    added PDF export

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def main():
 def project():
 	return flask.render_template('project.html', title="Yulia's Portfolio")
 
+
+@app.route('/exportCV')
+def exportCV():
+	return flask.send_from_directory(app.static_folder,'CV.pdf', as_attachment=True)
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
     #app.run()	
